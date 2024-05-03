8fcf33f6b24cdba61ef2414925e26187e32964c3
Adam Smelko <adamsmelko1@gmail.com>
2024-05-03 15:37:43 +0200

    add partition and cluster

--- benchmark/run-synth-mpi.sh
+++ benchmark/run-synth-mpi.sh
@@ -6,12 +6,12 @@ out_file=mpi_out_real.csv
 data_dir=data
 
 threads_to_test="16"
-mpi_nodes="1 2 4 8 16 32 64 96 128 160 192"
+mpi_nodes="1 2 4 8 16 32 64"
 
 mkdir -p ${results_dir}
 
 function runonce {
-  sbatch -N $2 --ntasks-per-node 8 --ntasks-per-socket 1 -c 16 --mem 64 -t 12:00:00  ${build_dir}/MaBoSS_1024n ${1}.bnd -c ${1}.cfg -e thread_count=16 -o ${build_dir}/res | cut -f2 -d' ' | tr '\n' ';' >> ${results_dir}/${out_file}${2}
+  sbatch -p batch --cluster aion -N $2 --ntasks-per-node 8 --ntasks-per-socket 1 -c 16 --mem 64 -t 12:00:00  ${build_dir}/MaBoSS_1024n ${1}.bnd -c ${1}.cfg -e thread_count=16 -o ${build_dir}/res | cut -f2 -d' ' | tr '\n' ';' >> ${results_dir}/${out_file}${2}
 }
 
 for m in ${mpi_nodes}
